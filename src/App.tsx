3b11981e8ecdb53365b69d1cbbb8ee4fe15688a0
NicholasNeto <nicholasneto@outlook.com>
2020-11-14 20:10:45 -0300

    created App and fix css for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,202 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "leaflet/dist/leaflet.css";
+
+import React, { FormEvent, useState } from "react";
+import { MapContainer } from "react-leaflet";
+import { Marker, Popup, TileLayer } from "react-leaflet";
+import Leaflet from "leaflet";
+import { v4 as uuidv4 } from "uuid";
+
+import { fetchLocalMapBox } from "./apiMapBox";
+import AsyncSelect from "react-select/async";
+
+import mapPackage from "./package.svg";
+import mapPin from "./pin.svg";
+
+import "./App.css";
+
+const initialPosition = { lat: -23.5838639, lng: -46.7572651 };
+
+const mapPackageIcon = Leaflet.icon({
+  iconUrl: mapPackage,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+const mapPinIcon = Leaflet.icon({
+  iconUrl: mapPin,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+interface Delivery {
+  id: string;
+  name: string;
+  address: string;
+  complement: string;
+  latitude: number;
+  longitude: number;
+}
+
+type Position = {
+  longitude: number;
+  latitude: number;
+};
 
 function App() {
+  const [deliveries, setDeliveries] = useState<Delivery[]>([]);
+
+  const [position, setPosition] = useState<Position | null>(null);
+
+  const [name, setName] = useState("");
+  const [complement, setComplement] = useState("");
+  const [address, setAddress] = useState<{
+    label: string;
+    value: string;
+  } | null>(null);
+
+  const [location, setLocation] = useState(initialPosition);
+
+  const loadOptions = async (inputValue: any, callback: any) => {
+    const response = await fetchLocalMapBox(inputValue);
+    let places: any = [];
+    if (inputValue.length < 5) return;
+    response.features.map((item: any) => {
+      places.push({
+        label: item.place_name,
+        value: item.place_name,
+        coords: item.center,
+        place: item.place_name,
+      });
+    });
+
+    callback(places);
+  };
+
+  const handleChangeSelect = (event: any) => {
+    console.log("changed", event);
+    setPosition({
+      longitude: event.coords[0],
+      latitude: event.coords[1],
+    });
+
+    setAddress({ label: event.place, value: event.place });
+
+    setLocation({
+      lng: event.coords[0],
+      lat: event.coords[1],
+    });
+  };
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if (!address || !name) return;
+
+    setDeliveries([
+      ...deliveries,
+      {
+        id: uuidv4(),
+        name,
+        address: address?.value || "",
+        complement,
+        latitude: location.lat,
+        longitude: location.lng,
+      },
+    ]);
+
+    setName("");
+    setAddress(null);
+    setComplement("");
+    setPosition(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-map">
+      <main>
+        <form onSubmit={handleSubmit} className="landing-page-form">
+          <fieldset>
+            <legend>Entregas</legend>
+
+            <div className="input-block">
+              <label htmlFor="name">Nome</label>
+              <input
+                id="name"
+                placeholder="Digite seu nome"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="address">Endereço</label>
+              <AsyncSelect
+                placeholder="Digite seu endereço..."
+                classNamePrefix="filter"
+                cacheOptions
+                loadOptions={loadOptions}
+                onChange={handleChangeSelect}
+                value={address}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="complement">Complemento</label>
+              <input
+                placeholder="Apto / Nr / Casa..."
+                id="complement"
+                value={complement}
+                onChange={(event) => setComplement(event.target.value)}
+              />
+            </div>
+          </fieldset>
+
+          <button className="confirm-button" type="submit">
+            Confirmar
+          </button>
+        </form>
+      </main>
+
+      <MapContainer
+        center={location}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX}`}
+        />
+
+        {position && (
+          <Marker
+            icon={mapPinIcon}
+            position={[position.latitude, position.longitude]}
+          ></Marker>
+        )}
+
+        {deliveries.map((delivery) => (
+          <Marker
+            key={delivery.id}
+            icon={mapPackageIcon}
+            position={[delivery.latitude, delivery.longitude]}
+          >
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              <div>
+                <h3>{delivery.name}</h3>
+                <p>
+                  {delivery.address} - {delivery.complement}
+                </p>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
