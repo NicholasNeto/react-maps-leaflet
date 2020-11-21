c07b65f29df23ba2bd92d49e02434cc8409b21af
NicholasNeto <nicholasneto@outlook.com>
2020-11-20 22:06:53 -0300

    add project App

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import { Marker, Popup, TileLayer } from "react-leaflet";
 import Leaflet from "leaflet";
 import { v4 as uuidv4 } from "uuid";
 
-import { fetchLocalMapBox } from "./apiMapBox";
+import { fetchLocalMapBox } from "./app/apiMapBox";
 import AsyncSelect from "react-select/async";
 
-import mapPackage from "./package.svg";
-import mapPin from "./pin.svg";
+import mapPackage from "../src/imagens/package.svg";
+import mapPin from "../src/imagens/pin.svg";
 
-import "./App.css";
+import "../src/css/App.css";
 
 const initialPosition = { lat: -23.5838639, lng: -46.7572651 };
 
@@ -35,6 +35,7 @@ interface Delivery {
   name: string;
   address: string;
   complement: string;
+  date: string;
   latitude: number;
   longitude: number;
 }
@@ -46,18 +47,22 @@ type Position = {
 
 function App() {
   const [deliveries, setDeliveries] = useState<Delivery[]>([]);
-
   const [position, setPosition] = useState<Position | null>(null);
 
+  const defaultDate = new Date();
+  const [isEditing, setIsEditing] = useState(false);
+  
+  const [id, setId] = useState('');
   const [name, setName] = useState("");
   const [complement, setComplement] = useState("");
+  const [date, setDate] = useState(defaultDate.toISOString());
   const [address, setAddress] = useState<{
     label: string;
     value: string;
   } | null>(null);
 
-  const [location, setLocation] = useState(initialPosition);
 
+  const [location, setLocation] = useState(initialPosition);
   const loadOptions = async (inputValue: any, callback: any) => {
     const response = await fetchLocalMapBox(inputValue);
     let places: any = [];
@@ -66,6 +71,7 @@ function App() {
       places.push({
         label: item.place_name,
         value: item.place_name,
+        hours: item.hours,
         coords: item.center,
         place: item.place_name,
       });
@@ -91,33 +97,90 @@ function App() {
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
+    console.log('handleSubmit')
 
     if (!address || !name) return;
 
+    let temporaryDeliverieses: Delivery[] = [...deliveries,]
+
+    let delivery: Delivery = {
+      id: uuidv4(),
+      name,
+      address: address?.value || "",
+      complement,
+      date,
+      latitude: location.lat,
+      longitude: location.lng,
+    }
+
+    if (isEditing) {
+      temporaryDeliverieses = makeChange(temporaryDeliverieses, delivery);
+    }
+
     setDeliveries([
-      ...deliveries,
-      {
-        id: uuidv4(),
-        name,
-        address: address?.value || "",
-        complement,
-        latitude: location.lat,
-        longitude: location.lng,
-      },
+      ...temporaryDeliverieses,
+      delivery,
     ]);
+
+    setId('')
+    setName("");
+    setAddress(null);
+    setComplement("");
+    setDate("");
+    setPosition(null);
+    setIsEditing(false);
+  }
+
+  function makeChange(temporaryDeliverieses: Delivery[], delivery: Delivery) {
+    temporaryDeliverieses = temporaryDeliverieses.map(it => {
+      if (it.id === id) {
+        it.id = id;
+        it.name = delivery.name;
+        it.address = delivery.address;
+        it.complement = delivery.complement;
+        it.date = delivery.date;
+        it.latitude = delivery.latitude;
+        it.longitude = delivery.longitude;
+      }
+      return it;
+    });
+    return temporaryDeliverieses;
+  }
+
+  async function handleFormReset(event: FormEvent) {
+    event.preventDefault();
 
     setName("");
     setAddress(null);
     setComplement("");
+    setDate("");
     setPosition(null);
+  }
+
+  async function modifierDelivery(event: any) {
+    event.preventDefault();
+
+    setIsEditing(true)
+    let found: Delivery | any = deliveries.find(it => it.id === event.target.value);
+
+    if (found.id) {
+      setId(found.id);
+    }
+  }
+
+  async function deletarDelivery(event: any) {
+
+    setDeliveries([
+      ...deliveries.filter(it => it.id !== event.target.value)
+    ]);
   }
 
   return (
     <div id="page-map">
       <main>
-        <form onSubmit={handleSubmit} className="landing-page-form">
+        <form onSubmit={handleSubmit} onReset={handleFormReset} className="landing-page-form">
           <fieldset>
-            <legend>Entregas</legend>
+            { isEditing ?  <legend>Alteração Dados</legend> : <legend>Entregas</legend>  }
 
             <div className="input-block">
               <label htmlFor="name">Nome</label>
@@ -150,12 +213,24 @@ function App() {
                 onChange={(event) => setComplement(event.target.value)}
               />
             </div>
+
+            <div className="input-block">
+              <label htmlFor="hours">Horas</label>
+              <input
+                id="date"
+                placeholder="dd/mm/yyyy"
+                value={date}
+                type="date"
+                onChange={(event) => setDate(event.target.value)}
+              />
+            </div>
+
           </fieldset>
 
-          <button className="confirm-button" type="submit">
-            Confirmar
-          </button>
+          <button className="confirm-button" type="submit" value="Submit">{isEditing ? "Confirmar Alteração" : "Confirmar"}</button>
+          <button className="confirm-reset" type="reset" value="Reset">{isEditing ? "Cancelar Alteração" : "Limpar"}</button>
         </form>
+
       </main>
 
       <MapContainer
@@ -189,9 +264,16 @@ function App() {
             >
               <div>
                 <h3>{delivery.name}</h3>
-                <p>
-                  {delivery.address} - {delivery.complement}
-                </p>
+
+                <h4>{'Endereço:'}</h4>
+                <p>{delivery.address} - {delivery.complement} </p>
+                <h4>{'Data de Entrega:'}</h4>
+                <p>{delivery.date}</p>
+
+
+                <button name='edit' className="edit-button" value={delivery.id} onClick={modifierDelivery} >Editar</button>
+                <button name='delete' className="delete-button" value={delivery.id} onClick={deletarDelivery} >Excluir</button>
+
               </div>
             </Popup>
           </Marker>
