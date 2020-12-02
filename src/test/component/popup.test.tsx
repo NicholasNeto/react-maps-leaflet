95a7b271192abb22ab3352f4ff99c5d41ff4914a
NicholasNeto <nicholasneto@outlook.com>
2020-12-02 06:47:46 -0300

    fix date for UNIT Testing the CustomPopup component

--- src/test/component/popup.test.tsx
+++ src/test/component/popup.test.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import moment from 'moment'
 import CustomPopup from '../../ui/component/popup';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
 
 configure({ adapter: new Adapter() });
+
+
+
 describe("[UNIT] Testing the CustomPopup component", () => {
-  let wrapper : any
+  let wrapper: any
+  let delivery: object
+  const date = moment()
 
   beforeEach(() => {
-    const delivery = {
+
+    delivery = {
       name: 'name test',
       address: 'address test',
       componen: 'componen test',
+      date: date,
     }
 
     wrapper = shallow(<CustomPopup delivery={delivery} />)
@@ -20,13 +29,18 @@ describe("[UNIT] Testing the CustomPopup component", () => {
   describe("Component validation", () => {
 
     it('Exists ClassName', () => {
-      expect(wrapper.exists('.map-popup')).toEqual(true);
-      expect(wrapper.exists('.edit-button')).toEqual(true);
-      expect(wrapper.exists('.delete-button')).toEqual(true);
+      expect(wrapper.exists('.map-popup')).to.equal(true);
+      expect(wrapper.exists('.edit-button')).to.equal(true);
+      expect(wrapper.exists('.delete-button')).to.equal(true);
     })
 
     it('displays 0 as a default value', () => {
-      expect(wrapper.find("h3").text()).toContain("name test")
+      expect(wrapper.find("h3").text()).to.contain("name test")
+    })
+
+    it('displays 0 as a default value', () => {
+      const texts = wrapper.find('p').map((node) => node.text());
+      expect(texts).to.eql(["address test -  ", date.format("MMM Do YY")]);
     })
   })
 })
